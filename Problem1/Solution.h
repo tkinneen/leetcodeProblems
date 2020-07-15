c36a84873b2e87fcda629565abaabd2394ac6038
tkinneen <kinneen@gmail.com>
2020-07-15 07:46:55 -0400

    cleanup for Problem 1 solution and initial files for Problem 8 solution

--- Problem1/Solution.h
+++ Problem1/Solution.h
@@ -3,17 +3,18 @@ public:
     
 	std::vector<int> twoSum(std::vector<int>& nums, int target) {
 		// A brute force solution would require nested for loops (exponential time complexity)
-		// The below solution is O(n) time complexity, and is achieved using a hash map 
+		// The below solution is O(n) time complexity, achieved by using a hash map 
 		
 		std::vector<int> answer;
 		std::unordered_map<int, int> valueMap;
 		
-		// Loop through all input elements, add to hash map
+		// Loop through all input elements, adding each to map
 		for(auto i = 0; i < nums.size(); i++) {
 		
-			// find the complement to the current number equalling the target	
+			// find the complement to the current number that equals the target	
 			int complement = target - nums[i];
-
+			
+			// If current value and complement add up to target, push each array element to answer vector 
 			if(valueMap.find(complement) != valueMap.end()) {
 				answer.push_back(valueMap[complement]);
 				answer.push_back(i);
@@ -22,6 +23,8 @@ public:
 			
        		valueMap[nums[i]] = i; 
 		}
+		// if no match is found still return vector
+		return answer;
 	}
 };
 
