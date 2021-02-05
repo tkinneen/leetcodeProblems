c43e4d358008ba8c766681968286b76bdd758d43
tkinneen <kinneen@gmail.com>
2021-02-04 21:43:29 -0500

    work in progress on Problem4 Python solution

---
+++ Problem4/Python/Solution.py
@@ -0,0 +1,8 @@
+class Solution:
+	#def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+	def findMedianSortedArrays(self, nums1, nums2):
+
+		print(nums1)
+		print(nums2)
+		
+		return 1.5
