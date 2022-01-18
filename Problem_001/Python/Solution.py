ba370a95dcd2c236bcfccfbf5bc2eded92e284e8
tkinneen <kinneen@gmail.com>
2022-01-17 21:47:46 -0500

    Added Problem 21 solution, also formatted Problem 1 for consistency

--- Problem_001/Python/Solution.py
+++ Problem_001/Python/Solution.py
@@ -1,25 +1,37 @@
+# 1. Two Sum
+# Given an array of integers nums and an integer target, return indices
+#   of the two numbers such that they add up to target.
+
+# You may assume that each input would have exactly one solution, and
+#   you may not use the same element twice.
+
+# You can return the answer in any order.
+
 class Solution:
-	def twoSum(self, nums, target):
+    def twoSum(self, nums, target):
 
-		# A brute force solution would use nested for loops (exponential time complexity)
-		# The below solution is O(n) time complexity, achieved by using a dictionary (hash map) 
-		
-		answer = [] # Will contain index postions of numbers that add up to the target
-		previousNumbers = {} # After each loop we add each iteration to the hash map for fast lookup
+        # Brute forcing would use nested for loops (exponential time complexity)
+        # The below hash map solution is O(n) time complexity
+        
+        # Contains index postions of numbers that add up to the target		
+        answer = [] 
 
-		for i, num in enumerate(nums):
+        # After we see a number we add it to the hash map
+        previousNumbers = {} 
+
+        for i, num in enumerate(nums):
 		
-			# Subtract the current number from the target number to find the complement value we need	
-			complement = target - num 
+            # Subtract the current number from the target number to find the complement value we need	
+            complement = target - num 
+
+            # If the current number's complement exists in the hash map, pack complement's index position and 
+            # the current index postion in the answer array
+            if complement in previousNumbers:
+                answer.append(previousNumbers[complement])
+                answer.append(i)
+                return answer
 		
-			# If the current number's complement exists in the hash map, pack complement's index position and 
-			# the current index postion in the answer array	
-			if complement in hashMap:
-				answer.append(hashMap[complement])
-				answer.append(i)
-				return answer
-		
-			# Add the current number to the has map if no answer has been found yet	
-			hashMap[num] = i
+            # Add the current number to the has map if no answer has been found yet
+            previousNumbers[num] = i
 			
-		return answer
+        return answer
--- Problem_001/Python/main.py
+++ Problem_001/Python/main.py
@@ -1,18 +1,18 @@
-# 1. Two Sum
-# Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
-# You may assume that each input would have exactly one solution, and you may not use the same element twice.
-# You can return the answer in any order.
-
 from Solution import Solution
 
 def main():
-	
-	testInputs = [2,7,11,15]
-	targetValue = 9
 
-	s = Solution() # instantiate Solution class
+    testInputs = [2,7,11,15]
+    targetValue = 9
 
-	answer = s.twoSum(nums, target)
+    # instantiate Solution class
+    s = Solution() 
+
+    answer = s.twoSum(testInputs, targetValue)
+
+    print(answer)
+
 
 if __name__ == "__main__":
 	main()
+
