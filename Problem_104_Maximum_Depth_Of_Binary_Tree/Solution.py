edfa3032f64cbf88d19a0bc647400a7dcc19fa49
tkinneen <kinneen@gmail.com>
2022-03-20 20:47:28 -0400

    Added solution with 3 different max binary tree depth implementations

--- Problem_104_Maximum_Depth_Of_Binary_Tree/Solution.py
+++ Problem_104_Maximum_Depth_Of_Binary_Tree/Solution.py
@@ -12,27 +12,85 @@ class TreeNode:
         self.right = right
 
 from typing import Optional
+from collections import deque
 
 class Solution:
     def maxDepth(self, root: Optional[TreeNode]) -> int:
 
-        # Time complexity is O(n) as is space complexity, because we recursively
-        #    traverse the entire tree node
+        # Time complexity is O(n) as we have to traverse the entire tree
+        # Space complexity is also worst case O(n) if it's not a balanced tree
 
-        # Base case for recursion - return 0 for null nodes
-        if not root:
-            return 0
+        # Recursive Depth First Search (DFS) Implementation
+        def RecursiveDFS(root):
+            
+            # Base case for recursion - return 0 for null nodes
+            if not root:
+                return 0
+            
+            # Recursively calculate the deepest child node. Each node will take the 
+            #    largetst value returned by its own children, then add 1 to represent
+            #    its own depth and return that to its parent
+            currentMax = 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
 
-        print(str(root.val) + " ===============open")
-        print("Current node: " + str(root.val))
-        # Recursively calculate the deepest child node, add 1 for the current node's
-        #    level, then return the combined value to the next node up the tree
-        returning = 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
-        print("node " + str(root.val) + " returning: " + str(returning))
-        print(str(root.val) + " ===============close")
-
-
-        return returning
-
+            return currentMax
         
-        #return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
+        # Breadth First Search (BFS) Implementation
+        def BreadthFirstSearch(root):
+            
+            # Base case for recursion
+            if not root:
+                return 0
+            
+            # St
+            level = 0
+            
+            # Deque allows us to efficiently pop from either the left or right of queue
+            queue = deque([root])
+            
+            # Loop as long as nodes exist in the deque
+            while queue:
+                # 
+                for i in range(len(queue)):
+                    # Pop from the deque in the order the nodes were added
+                    node = queue.popleft()
+                    # Append both child nodes to the deque, regardless of whether they are Null
+                    queue.append(node.left)
+                    queue.append(node.right)
+                # Increment the level after each wave of nodes
+                level += 1
+            return level
+            
+        # Iterative Depth First Search (DFS) Implementation
+        def iterativeDFS(root):
+            
+            # Base case for recursion
+            if not root:
+                return 0
+            
+            # Store the node as well as its depth in the stack
+            stack = [[root, 1]]
+            
+            # Continually increment the max depth 
+            maxDepth = 1
+
+            # Continue looping while there are nodes in the stack
+            while stack:
+                
+                # Grabs the last-added node and its depth from the stack
+                node, depth = stack.pop()    
+            
+                if node:
+                    # Updates the max depth val if current node's depth is greater
+                    maxDepth = max(maxDepth, depth)
+                    # Appends the children of the current node to the stack (they may be null)
+                    stack.append([node.left, depth + 1])
+                    stack.append([node.right, depth + 1])
+
+            # Return the maxDepth when there are no more nodes in the stack
+            return maxDepth
+
+        # 3 different implementations, all return the same answer
+        return RecursiveDFS(root)
+        #return BreadthFirstSearch(root)
+        #return iterativeDFS(root)           
+
--- Problem_104_Maximum_Depth_Of_Binary_Tree/main.py
+++ Problem_104_Maximum_Depth_Of_Binary_Tree/main.py
@@ -2,14 +2,23 @@ from Solution import TreeNode, Solution
 
 def main():
     
-    # Example 1
-    # Create first list node and set the head pointer there
+    # Build test tree
     treeNodeHead = testTree = TreeNode(3)
     testTree.left = TreeNode(9)
     testTree.right = TreeNode(20)
     testTree = testTree.right
     testTree.left = TreeNode(15)
     testTree.right = TreeNode(7)
+    placeholder = testTree
+    testTree = testTree.left
+    testTree.left = TreeNode(77)
+    testTree.right = TreeNode(88)
+    testTree = placeholder
+    testTree = testTree.right
+    testTree.left = TreeNode(33)
+    testTree.right = TreeNode(44)
+
+    # Point to the head before passing to the Solution class
     testTree = treeNodeHead
     
     # Instantiate Solution class
@@ -17,7 +26,7 @@ def main():
 
     # Run the solution
     answer = s.maxDepth(testTree)
-    print("Max depth of binary tree: " + str(answer))
+    print("Max depth of the binary tree: " + str(answer))
 
     
 if __name__ == "__main__":
