8b7b66b906699d4a3fb3893709b119956396829d
tkinneen <kinneen@gmail.com>
2023-06-05 00:08:51 -0400

    Optimization for Two Sum solution

--- Problem_001_Two_Sum/Solution.py
+++ Problem_001_Two_Sum/Solution.py
@@ -5,31 +5,26 @@
 #    you may not use the same element twice.
 # You can return the answer in any order.
 
+
 class Solution:
     def twoSum(self, nums, target):
-
         # Brute forcing would use nested for loops (exponential time complexity)
         # The below hash map solution is O(n) time complexity
-        
-        # Contains index postions of numbers that add up to the target		
-        answer = [] 
 
         # After we see a number we add it to the hash map
-        previousNumbers = {} 
+        seenNums = {}
 
-        for i, num in enumerate(nums):
-		
-            # Subtract the current number from the target number to find the complement value we need	
-            complement = target - num 
+        for i, curNum in enumerate(nums):
+            # Subtract the current number from the target number to find the complement value we need
+            complement = target - curNum
 
-            # If the current number's complement exists in the hash map, pack complement's index position and 
+            # If the current number's complement exists in the hash map, pack complement's index position and
             # the current index postion in the answer array
-            if complement in previousNumbers:
-                answer.append(previousNumbers[complement])
-                answer.append(i)
-                return answer
-		
-            # Add the current number to the has map if no answer has been found yet
-            previousNumbers[num] = i
-			
-        return answer
+            if complement in seenNums:
+                return [seenNums[complement], i]
+            else:
+                # Otherwise, add the current number and its index to the hashmap
+                seenNums[curNum] = i
+
+        # No answer was found
+        return []
