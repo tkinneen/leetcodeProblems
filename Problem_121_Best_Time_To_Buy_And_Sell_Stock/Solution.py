0ff2caa0c8bce9756c75c33cc5461ed8f98f60c8
tkinneen <kinneen@gmail.com>
2023-06-06 19:26:11 -0400

    Cleanups and slight optimizations to two sum II and best time to buy stock

--- Problem_121_Best_Time_To_Buy_And_Sell_Stock/Solution.py
+++ Problem_121_Best_Time_To_Buy_And_Sell_Stock/Solution.py
@@ -2,16 +2,16 @@
 
 # You are given an array prices where prices[i] is the price of a given
 #    stock on the ith day.
-# You want to maximize your profit by choosing a single day to buy one 
+# You want to maximize your profit by choosing a single day to buy one
 #    stock and choosing a different day in the future to sell that stock.
-# Return the maximum profit you can achieve from this transaction. If you 
+# Return the maximum profit you can achieve from this transaction. If you
 #    cannot achieve any profit, return 0.
 
 from typing import List
 
+
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-
         # Time complexity: O(n) - Memory complexity: O(1) (as we're only using pointers)
 
         # This is called a "two pointer" solution
@@ -20,20 +20,17 @@ class Solution:
 
         # Continue looping while rightPtr has not exceeded the list bounds
         while rightPtr < len(prices):
-            
             # Left is "buy" price, right is "sell" price. Sell price needs to be higher then buy.
             if prices[leftPtr] < prices[rightPtr]:
-
                 # Calculate the profit from this transaction
-                profit = prices[rightPtr] - prices[leftPtr]
-                
+                curProfit = prices[rightPtr] - prices[leftPtr]
+
                 # Update the maxProfit variable if the latest profit exceeds the current max
-                if profit > maxProfit:
-                    maxProfit = profit
+                maxProfit = max(curProfit, maxProfit)
             else:
                 # If the "buy" price is higher than the "sell" price, shift the left pointer to the right's current position
                 leftPtr = rightPtr
-            
+
             # Step the right pointer to the next value regardless
             rightPtr += 1
 
--- Problem_167_Two_Sum_II/Solution.py
+++ Problem_167_Two_Sum_II/Solution.py
@@ -1,41 +1,43 @@
 # LeetCode Problem 167: Two Sum II
-# Given a 1-indexed array of integers numbers that is already sorted in 
-#    non-decreasing order, find two numbers such that they add up to a 
-#    specific target number. Let these two numbers be numbers[index1] and 
+# Given a 1-indexed array of integers numbers that is already sorted in
+#    non-decreasing order, find two numbers such that they add up to a
+#    specific target number. Let these two numbers be numbers[index1] and
 #    numbers[index2] where 1 <= index1 < index2 <= numbers.length.
 
-# Return the indices of the two numbers, index1 and index2, added by one as 
+# Return the indices of the two numbers, index1 and index2, added by one as
 #    an integer array [index1, index2] of length 2.
 
-# The tests are generated such that there is exactly one solution. You may 
+# The tests are generated such that there is exactly one solution. You may
 #    not use the same element twice.
 
 # Your solution must use only constant extra space.
 
-class Solution:
-    def twoSum(self, nums, target):
+from typing import List
 
-        # As we know the array is sorted, we can use a 2 pointer solution		
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> int:
+        # As we know the array is sorted, we can use a 2 pointer solution
         leftPtr = 0
         rightPtr = len(nums) - 1
 
         # Loop until the two pointers meet
         while leftPtr < rightPtr:
             # Sum the two values at the L and R ptr array positions
-            curValue = nums[leftPtr] + nums[rightPtr]
-            
-            # If current val overshoots the target, we move the R ptr one position 
+            twoSum = nums[leftPtr] + nums[rightPtr]
+
+            # If current val overshoots the target, we move the R ptr one position
             #    to the left, as we know it will be smaller than the current R ptr
-            if curValue > target:
+            if twoSum > target:
                 rightPtr -= 1
-            # If the current val undershoots, we move the L ptr to the right 
-            elif curValue < target:
-                leftPtr += 1 
+            # If the current val undershoots, we move the L ptr to the right
+            elif twoSum < target:
+                leftPtr += 1
             # If current hits our target, return the L and R ptr postions.
             # This question considers first element in the array as 1 for some reason,
             #    so we bump both ptr positions by 1
-            elif nums[leftPtr] + nums[rightPtr] == target:
+            elif twoSum == target:
                 return [leftPtr + 1, rightPtr + 1]
 
-		# The question guarantees exactly one solution, so we never get here
+        # The question guarantees exactly one solution, so we never get here
         return []
