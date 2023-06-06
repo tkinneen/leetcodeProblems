4c2a209345822e14d05f0fa00cb21f8182f43d65
tkinneen <kinneen@gmail.com>
2023-06-05 23:51:05 -0400

    More logging for problem 338

--- Problem_338_Counting_Bits/Solution.py
+++ Problem_338_Counting_Bits/Solution.py
@@ -8,11 +8,15 @@ from typing import List
 
 class Solution:
     def countBits(self, n: int) -> List[int]:
-        dp = [0] * (n + 1)
-        print(f"dp at the outset: {dp}")
-        # result = [0]
+        # Pad the array from 0 through the passed-in input
+        result = [0] * (n + 1)
+        print(f"dp at the outset: {result}")
+
+        #
         offset = 1
 
+        # Loop
+        # Start at 1 because we padded entire array w/ 0's and 0 is 0
         for i in range(1, n + 1):
             print(f"i: {i}")
 
@@ -20,10 +24,12 @@ class Solution:
                 offset = i
                 print(f"offset getting updated to: {i}")
 
-            print(f"dp[i - offset]: {dp[i - offset]}")
+            print(f"result[i - offset]: {result[i - offset]}")
+            print(f"offset: {offset}")
+            print(f"i - offset: {i - offset}")
 
-            dp[i] = 1 + dp[i - offset]
-            print(f"dp: {dp}")
+            result[i] = 1 + result[i - offset]
+            print(f"result: {result}")
             print(f"=====================")
 
-        return dp
+        return result
