4d940c20d64d5751834ec9924c6e487c61888293
tkinneen <kinneen@gmail.com>
2023-06-06 23:17:17 -0400

    Added heavy logging to max product subarray to try to understand the solution

--- Problem_152_Maximum_Product_Subarray/Solution.py
+++ Problem_152_Maximum_Product_Subarray/Solution.py
@@ -1,47 +1,51 @@
 # LeetCode Problem 152: Maximum Product Subarray
 #
-# Given an integer array nums, find a subarray that has the largest product, and 
+# Given an integer array nums, find a subarray that has the largest product, and
 #    return the product.
 #
 # The test cases are generated so that the answer will fit in a 32-bit integer.
 
 from typing import List
 
+
 class Solution:
     def maxProduct(self, nums: List[int]) -> int:
         
-        print(f"nums: {nums}")
+        # Time complexity: O(n) - Entire array must be traversed
+        # Memory complexity: O(1) - We're only tracking individual variables
 
-        # Initially set the original result to the largest number in the array
+        print(f"nums at the outset: {nums}")
+
+        # Initialize the result to the largest number in the array
         result = max(nums)
 
+        # Initialize to neutral 1's, they'll become whatever they're multiplied by
+        curMin, curMax = 1, 1
+
         # 
-        currentMin, currentMax = 1, 1
+        for curNum in nums:
+            print(f"top of the loop: curMax: {curMax}, curMin: {curMin}, curNum: {curNum}")
 
-        for currentNum in nums:
-            
-            # Aparently not necessary
-            #if currentNum == 0:
-            #    currentMin, currentMax = 1, 1
-            #    continue
-            
-            # Store this value; we need it to calculate both currentMax and currentMin, 
-            #    and it will change after we calculate currentMax
-            temp = currentNum * currentMax
+            # This value is needed to calculate both currentMax and currentMin,
+            #    and
+            temp = curNum * curMax
+            print(f"curNum ({curNum}) * curMax ({curMax}): {temp}")
 
-            # 
-            currentMax = max(currentNum * currentMax, currentNum * currentMin, currentNum)
-            currentMin = min(temp, currentNum * currentMin, currentNum)
+            #
+            print(f"taking max of (curNum * curMax) {curNum * curMax}, (curNum * curMin) {curNum * curMin}, and (curNum) {curNum}")
+            print(f"taking min of (curNum * curMax) {temp}, (curNum * curMin) {curNum * curMin}, and (curNum) {curNum}")
 
-            print(f"currentMax: {currentMax}")
-            print(f"currentMin: {currentMin}")
+            curMax = max(curNum * curMax, curNum * curMin, curNum)
+            curMin = min(temp, curNum * curMin, curNum)
 
+            print(f"curMax: {curMax}, curMin: {curMin}")
 
-            # As necessary update the result if the currentMax is greater
-            result = max(result, currentMax)
+            # Update the result if currentMax exceeds it
+            result = max(result, curMax)
 
-            print(f"result: {result}")
+            print(f"running result: {result}")
             print(f"========")
 
-        
+        print(f"final result: {result}")
+
         return result
