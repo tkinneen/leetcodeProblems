2aabaadd4d4d52a99e2d5ba8154780f110e49448
tkinneen <kinneen@gmail.com>
2024-11-09 11:07:38 -0500

    Added main file for problem 3 (longest substring) with full test strings

--- Problem_003_Longest_Substring_Without_Repeating_Characters/main.py
+++ Problem_003_Longest_Substring_Without_Repeating_Characters/main.py
@@ -2,7 +2,9 @@ from Solution import Solution
 
 def main():
 
-    testString = "abcabcbb"
+    testString = "abcabcbb" # Output: 3
+    testString = "bbbbb" # Output: 1
+    testString = "pwwkew" # Output: 3
 
     # instantiate Solution class
     s = Solution()
