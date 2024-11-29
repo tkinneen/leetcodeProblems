4b22d8ca2d60ef241bd97b116d6eeb4a25c4da55
tkinneen <kinneen@gmail.com>
2024-11-29 16:39:49 -0500

    Added time/space complexity to problem 235

--- Problem_235_Lowest_Common_Ancestor_of_a_Binary_Search_Tree/Solution.py
+++ Problem_235_Lowest_Common_Ancestor_of_a_Binary_Search_Tree/Solution.py
@@ -14,23 +14,27 @@ class TreeNode:
         self.left = None
         self.right = None
 
+from typing import Optional
+
 class Solution:
-    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+    def lowestCommonAncestor(self, root: Optional[TreeNode], p: Optional[TreeNode], q: Optional[TreeNode]) -> Optional[TreeNode]:
+
+        # Time: O(h) - explores the height of the tree in worst case
+        # Space: O(1) - simply tracks the current node regardless of tree size
 
         # Start at the root; it will always be a common ancestor of every node in the tree, though not necessarily the lowest
         curNode = root
 
         # Loop until the solution is found
         while curNode:
-
             # Both p and q are greater than the current node, so both reside in the right subtree.
             #   Shift current node to the right child and continue search
             if p.val > curNode.val and q.val > curNode.val:
                 curNode = curNode.right
             # Both p and q are less than the current node, so both reside in the left subtree.
-            # Shift current node to the left child and continue search
+            #   Shift current node to the left child and continue search
             elif p.val < curNode.val and q.val < curNode.val:
                 curNode = curNode.left
-            # p and q reside in separate subtrees, so the current node is the lowest possible common ancestor
+            # p and q reside in separate subtrees, so the current node is the lowest possible common ancestor (the split)
             else:
                 return curNode
